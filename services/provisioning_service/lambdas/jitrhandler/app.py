2ede1eaf30dc05767942c73bfab22c2ead79ab6a
Simith Nambiar <simithn@amazon.com>
2021-02-07 22:20:13 +0800

    adding the jitr lambda function

---
+++ services/provisioning_service/lambdas/jitrhandler/app.py
@@ -0,0 +1,6 @@
+
+def on_jitr_request_handler(event,context):
+    print("processing request in provisioning api")
+
+
+
--- services/provisioning_service/lambdas/provisioningapi/app.py
+++ services/provisioning_service/lambdas/provisioningapi/app.py
@@ -1,3 +1,7 @@
 
 def on_request(event,context):
     print("processing request in provisioning api")
+
+
+def on_jitr_request_handler(event,context):
+    print("processing request in provisioning api")
