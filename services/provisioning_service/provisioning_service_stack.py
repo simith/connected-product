37df399b65b67806ae65659bd7945bfe8c8b352f
Simith Nambiar <simithn@amazon.com>
2021-02-07 22:03:45 +0800

    adding the jitr lambda function

--- services/provisioning_service/provisioning_service_stack.py
+++ services/provisioning_service/provisioning_service_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import core
 import aws_cdk.aws_lambda as _lambda
 from aws_cdk.core import CfnOutput,Duration
+import aws_cdk.aws_iot as iot
+from aws_cdk.aws_iot import CfnTopicRule
 
 from utils.cp_utils import CPUtils
 
@@ -26,6 +28,9 @@ class ProvisioningServiceStack(core.Stack):
             code=_lambda.Code.asset('services/provisioning_service/lambdas/provisioningapi'),
             handler='app.on_jitr_request_handler') 
 
+        self.createJitrResources(lambda_fn_jitr.function_arn)
+        
+
         # The code that defines your stack goes here
         utils = CPUtils()
         provisioning_api = utils.create_api_gateway(self,"provisioning-service",
@@ -34,4 +39,18 @@ class ProvisioningServiceStack(core.Stack):
                                 enable_proxy=True) 
 
         s3_bucket = utils.create_s3_bucket(self,"provisioning-models");    
+
+    
+    
+    def createJitrResources(self, handler_function_arn):
+        ''' Steps:
+            create the lambda handle
+            capture the IoT Rule and set the Lambda action
+        '''
+        topic_rule = iot.CfnTopicRule(self,
+                                      id="lorawanRegistrationTrigger",
+                                      topic_rule_payload=iot.CfnTopicRule.TopicRulePayloadProperty(actions=[iot.CfnTopicRule.ActionProperty(lambda_=iot.CfnTopicRule.LambdaActionProperty(function_arn=handler_function_arn))],
+                                                                                                   sql="SELECT * FROM '$aws/events/thing/+/created'",
+                                                                                                   aws_iot_sql_version="2016-03-23",
+                                                                                                   rule_disabled=False), rule_name="loraWANTriggerRule")
 
